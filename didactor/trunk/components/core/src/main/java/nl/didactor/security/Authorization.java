dc768d03aaf95f535662318eca3309e8b262ccfa
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-12-02 14:09:04 +0000

    some details

--- didactor/trunk/components/core/src/main/java/nl/didactor/security/Authorization.java
+++ didactor/trunk/components/core/src/main/java/nl/didactor/security/Authorization.java
@@ -5,9 +5,12 @@ import nl.didactor.builders.DidactorBuilder;
 import nl.didactor.builders.DidactorRel;
 import org.mmbase.security.SecurityException;
 import org.mmbase.security.*;
+import org.mmbase.bridge.*;
+import org.mmbase.storage.search.Step;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 import org.mmbase.module.core.*;
+import org.mmbase.module.corebuilders.InsRel;
 
 
 /**
@@ -19,28 +22,33 @@ public class Authorization extends org.mmbase.security.Authorization {
     private static final Logger log = Logging.getLoggerInstance(Authorization.class);
 
     private static final String EVERYBODY = "everybody";
-    private static final Set    possibleContexts = Collections.unmodifiableSet(new HashSet(Arrays.asList( new String[]{EVERYBODY, "owner"})));
+    private static final Set<String>  possibleContexts = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList( new String[]{EVERYBODY, "owner"})));
+    private static final Set<String>  privateTypes     = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList( new String[]{"problems"})));
 
     /**
      *	This method does nothing
      */
+    @Override
     protected void load() {
     }
 
     /**
      *	This method does nothing
      */
+    @Override
     public void create(org.mmbase.security.UserContext user, int nodeid) {
     }
 
     /**
      *	This method does nothing
      */
+    @Override
     public void update(org.mmbase.security.UserContext user, int nodeid) {
     }
 
     /**
      */
+    @Override
     public boolean check(org.mmbase.security.UserContext user, int nodeid, Operation operation) {
         if (! (user instanceof nl.didactor.security.UserContext)) {
             return false;
@@ -48,16 +56,42 @@ public class Authorization extends org.mmbase.security.Authorization {
             // This is in no way an elaborate implementation
 
             nl.didactor.security.UserContext uc = (nl.didactor.security.UserContext) user;
+
+            MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
+            MMObjectNode node = objectBuilder.getNode(nodeid);
+            if (node == null) {
+                return true;
+            }
+
+            String ownerField = user.getOwnerField();
+            if (uc.getRank().getInt() <= Authentication.DIDACTOR_USER.getInt()) {
+                if (operation.equals(Operation.DELETE) ||
+                    (operation.equals(Operation.WRITE) && uc.getUserNumber() != nodeid) ||
+                    (operation.equals(Operation.READ) && privateTypes.contains(objectBuilder.getTableName()))) {
+                    String context = getContext(user, nodeid);
+                    boolean access = context.equals(ownerField);
+                    if (! access && node.getBuilder() instanceof InsRel) {
+                        // for relations we grant access if you have access to at least one of the end points.
+                        String sourceContext = getContext(user, node.getIntValue("snumber"));
+                        String destinationContext = getContext(user, node.getIntValue("dnumber"));
+                        access = sourceContext.equals(ownerField) || destinationContext.equals(ownerField);
+                    }
+                    if (! access) {
+                        log.warn("Access denied to " + user + " (" + user.getOwnerField() + ") for " + operation + " on " + node + " (" + context + ")");
+                    }
+                    return access;
+                }
+
+            }
+
+
             if (operation.equals(Operation.DELETE)) {
                 if (uc.getUserNumber() == nodeid) {
                     // you may not delete yourself
                     return false;
-                } 
-                MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
-                MMObjectNode node = objectBuilder.getNode(nodeid);
-                if (node == null) {
-                    return true;
                 }
+
+
                 if (node.getBuilder().getTableName().equals("people")) {
                     try {
                         UserContext otherUser = new UserContext(node, "check");
@@ -72,15 +106,16 @@ public class Authorization extends org.mmbase.security.Authorization {
                         return true;
                     }
                 }
-                
+
             }
         }
         return true;
     }
 
     /**
-     * This method will call the 'preDelete()' method for the builder to which this node that is deleted belongs. 
+     * This method will call the 'preDelete()' method for the builder to which this node that is deleted belongs.
      */
+    @Override
     public void verify(org.mmbase.security.UserContext user, int nodeid, Operation operation) throws org.mmbase.security.SecurityException {
         super.verify(user, nodeid, operation);
         if (operation.equals(Operation.DELETE)) {
@@ -101,6 +136,7 @@ public class Authorization extends org.mmbase.security.Authorization {
     /**
      * This method does nothing
      */
+    @Override
     public void remove(org.mmbase.security.UserContext user, int nodeid) {
 
     }
@@ -120,6 +156,7 @@ public class Authorization extends org.mmbase.security.Authorization {
                 }
             }
         }
+
         return true;
     }
 
@@ -128,16 +165,37 @@ public class Authorization extends org.mmbase.security.Authorization {
     /**
      * This method does nothing, except from giving a specified string back
      */
+    @Override
     public String getContext(org.mmbase.security.UserContext user, int nodeNumber) throws SecurityException {
-        MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
-        MMObjectNode node = objectBuilder.getNode(nodeNumber);
-        return org.mmbase.util.Casting.toString(node.getValues().get("owner"));
+        Node node = LocalContext.getCloudContext().getCloud("mmbase", user).getNode(nodeNumber);
+        if (node.getNodeManager().hasField("context")) {
+            Object context = node.getValue("context");
+            if(context != null) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Type of context " + context.getClass() + " " + context);
+                }
+                if (context instanceof Node) {
+                    return ((org.mmbase.bridge.Node) context).getContext();
+                } else {
+                    return org.mmbase.util.Casting.toString(context);
+                }
+            } else {
+                MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
+                MMObjectNode mnode = objectBuilder.getNode(nodeNumber);
+                return org.mmbase.util.Casting.toString(mnode.getValues().get("owner"));
+            }
+        } else {
+            MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
+            MMObjectNode mnode = objectBuilder.getNode(nodeNumber);
+            return org.mmbase.util.Casting.toString(mnode.getValues().get("owner"));
+        }
     }
 
     /**
      * Since this is not authorization, we simply allow every change of context.
      */
-    public void setContext(org.mmbase.security.UserContext user, int nodeNumber, String context) throws SecurityException {        
+    @Override
+    public void setContext(org.mmbase.security.UserContext user, int nodeNumber, String context) throws SecurityException {
         MMObjectBuilder objectBuilder = MMBase.getMMBase().getBuilder("object");
         MMObjectNode node = objectBuilder.getNode(nodeNumber);
         node.setValue("owner", context);
@@ -146,12 +204,24 @@ public class Authorization extends org.mmbase.security.Authorization {
     /**
      * This method does nothing, except from returning a dummy value
      */
+    @Override
     public Set getPossibleContexts(org.mmbase.security.UserContext user, int nodeid) throws SecurityException {
-        return possibleContexts;
+        Set<String> result = new HashSet<String>(possibleContexts);
+        result.add(getContext(user, nodeid)); // add current context too
+        return result;
     }
 
+    @Override
     public QueryCheck check(org.mmbase.security.UserContext user, org.mmbase.bridge.Query query, Operation operation) {
+        if (user.getRank().getInt() <= Authentication.DIDACTOR_USER.getInt()) {
+            for (Step step : query.getSteps()) {
+                if (privateTypes.contains(step.getTableName())) {
+                    return NO_CHECK;
+                }
+            }
+        }
         return COMPLETE_CHECK;
+
     }
 
 }
